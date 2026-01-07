9aaf2a8aed52d98d215c035afbbb4f625b2e6188
sash574 <sash574@nomail.de>
2026-01-07 19:37:01 +0100

    GUI without filtered selection

---
+++ README.md
@@ -0,0 +1,27 @@
+# Friends Sitcom Episode Selector
+
+This is a fun little project I implemented because I am a big fan of the Friends sitcom. Since the TV show is my all-time favorite comfort series, I wanted to implement a little episode selector to have some fun with choosing what episodes to watch.
+
+![Image 1: GUI - Main Window](images/main_window.png)
+
+Also, this was my first dive into GUI development (in general and with Tkinter), so I took it as a learning opportunity - but do not expect professional or perfect code here.
+
+The data used is from Kaggle: https://www.kaggle.com/datasets/thebumpkin/ultimate-friends-tv-sitcom-dataset?resource=download
+
+It contains information on the season, episode number (both seasonally and overall), the episodes title and description as well as the director, the writer, the air date and a link to the IMDb page with further information.
+
+## Implementation
+
+The repository contains a `requirements.txt` file that can be run to install all needed packages. Also, you need to ensure that Tkinter works on your machine (should be installed already with your Python version, otherwise: sudo apt install python3-tk (Linux Ubuntu)). 
+
+Afterwards, the program works by simply running the main Python script `episode_selector.py`. This then opens a graphical user interface (= GUI) where one can randomly chose an episode to watch and play around with an image gallery.    
+The program also contains an option to display the chosen episode and its details in a pop-up window instead of in the main window. You can find more information on this in the main code file.
+
+![Image 2: GUI - Main Window After Selecting an Episode](images/main_window_after_buttonclick.png)
+
+## To Dos
+
+- [x] randomized episode selection
+- [x] create GUI
+- [x] pipe requirements.txt
+- [ ] code filtered, randomized episode selection (i.e. from a specific season or based on contents of the description, specific directors etc.)
---
+++ episode_selector.py
@@ -0,0 +1,179 @@
+import pandas as pd
+import random
+import tkinter
+import tkinter.messagebox
+from PIL import Image, ImageTk
+
+
+##### main algorithms ######
+
+def button_click():
+
+    # open data
+    filepath = 'data/FriendsEpisodes.csv'
+    df = pd.read_csv(filepath)
+
+    # randomly select episode number; no filter
+    random_range = len(df)
+    random_episode_number = random.randrange(1, random_range + 1)
+
+    ## find the episode in the DataFrame
+    df_episode = df[df['episode'] == random_episode_number]
+
+    ## check if the episode DataFrame is empty, otherwise print episode details
+    if df_episode.empty:
+        episode_details = f'Episode number {random_episode_number} not found.'
+
+    else:
+        df_episode = df_episode.iloc[0]
+        season = df_episode['season']
+        number = df_episode['number']
+        title = df_episode['title']
+        director = df_episode['director']
+        writer = df_episode['writer']
+        air_date = df_episode['air_date']
+        description = df_episode['description']
+        link = df_episode['link']
+
+        episode_details = (
+            f'\nChosen Episode - Details:\n\n'
+            f'Season: {season}\n'
+            f'Episode Number: {number}\n'
+            f'\nTitle: {title}\n'
+            f'Director: {director}\n'
+            f'Writer: {writer}\n'
+            f'Air Date: {air_date}\n'
+            f'Description: {description}\n'
+            f'\nMore Information: {link}'
+        )
+
+    episode_label.config(text=episode_details)
+    closing_label.config(text='\nAre you ready to enter the Friends-universe? Have fun!\nAnd if not: Just click the button again to select another episode or apply filters.')
+
+def button_click_popup():
+
+    # open data
+    filepath = 'data/FriendsEpisodes.csv'
+    df = pd.read_csv(filepath)
+
+    # randomly select episode number; no filter
+    random_range = len(df)
+    random_episode_number = random.randrange(random_range)
+
+    ## find the episode in the DataFrame
+    df_episode = df[df['episode'] == random_episode_number]
+
+    ## check if the episode DataFrame is empty, otherwise print episode details
+    if df_episode.empty:
+        episode_details = f'Episode number {random_episode_number} not found.'
+
+    else:
+        df_episode = df_episode.iloc[0]
+        season = df_episode['season']
+        number = df_episode['number']
+        title = df_episode['title']
+        director = df_episode['director']
+        writer = df_episode['writer']
+        air_date = df_episode['air_date']
+        description = df_episode['description']
+        link = df_episode['link']
+
+        episode_details = (
+            f'Chosen Episode:\n\n'
+            f'Season: {season}\n'
+            f'Episode Number: {number}\n'
+            f'Title: {title}\n'
+            f'Director: {director}\n'
+            f'Writer: {writer}\n'
+            f'Air Date: {air_date}\n'
+            f'Description: {description}\n'
+        )
+
+    tkinter.messagebox.showinfo('Chosen Episode Details',  episode_details, detail=f'More Information: {link}', icon='info')
+
+
+##### helper functions for design implementations #####
+
+def show_image(img_no):
+    global current_img
+    current_img = img_no
+    image_label.config(image=list_images[img_no - 1])
+
+    # enable/disable buttons at the ends
+    if img_no == 1:
+        button_back.config(state='disabled')
+    else:
+        button_back.config(state='normal')
+
+    if img_no == len(list_images):
+        button_forward.config(state='disabled')
+    else:
+        button_forward.config(state='normal')
+
+def forward():
+    show_image(current_img + 1)
+
+def back():
+    show_image(current_img - 1)
+
+
+##### GUI development ######
+
+# initialize and customize root window
+root = tkinter.Tk()
+root.title('Friends Episode Selector')
+root.geometry('1000x1000')
+
+# add images as gallery
+image_no_1 = ImageTk.PhotoImage(Image.open('images/2066889.jpg').resize((500, 350)))
+image_no_2 = ImageTk.PhotoImage(Image.open('images/1898573.jpg').resize((500, 350)))
+image_no_3 = ImageTk.PhotoImage(Image.open('images/1810196.jpg').resize((500, 350)))
+image_no_4 = ImageTk.PhotoImage(Image.open('images/1810247.jpg').resize((500, 350)))
+
+list_images = [image_no_1, image_no_2, image_no_3, image_no_4]
+current_img = 1
+
+    # create a frame for the image gallery
+gallery_frame = tkinter.Frame(root)
+gallery_frame.pack(anchor='center', pady=20)
+
+    # image label inside the frame
+image_label = tkinter.Label(gallery_frame, image=image_no_1)
+image_label.pack()
+
+    # source label inside the frame
+source_label = tkinter.Label(gallery_frame, text='Source: https://wallpaperaccess.com/friends-series', anchor='center', fg='grey')
+source_label.pack()
+
+    # buttons frame inside the frame
+button_frame = tkinter.Frame(gallery_frame)
+button_frame.pack()
+button_back = tkinter.Button(button_frame, text='←', command=back, state='disabled')
+button_forward = tkinter.Button(button_frame, text='→', command=forward)
+button_back.pack(side='left', padx=10)
+button_forward.pack(side='left', padx=10)
+
+# introductory text
+label_text = 'Welcome to the episode selector for the popular sitcom Friends.\n\nHave you ever sat before your laptop and not been able to choose an episode? That is now over, thanks to this program. Simply click on the button and have the perfect episode chosen for you!\nAnd if you want some more control, you can even apply filters.\n\nHave fun!\n'
+introduction_text = tkinter.Label(root, text=label_text, anchor='center', fg='darkblue', wraplength=600)
+introduction_text.pack()
+
+# button for episode selection 
+    ## change to command=button_click_popup for popup instead of label text display of the episode details
+button = tkinter.Button(root, text='Click me to select an episode!', bg='orange', fg='white', activebackground='yellow', width=50, command=button_click)
+button.pack()
+
+    # episode details (filled after button click)
+episode_details = ''
+
+episode_label = tkinter.Label(root, text=episode_details, anchor='center', fg='black', wraplength=600)
+episode_label.pack()
+
+    # closing statement (filled after button click)
+closing_statement = ''
+
+closing_label = tkinter.Label(root, text=closing_statement, anchor='center', fg='darkred', wraplength=600)
+closing_label.pack()
+
+# start the main event loop
+root.mainloop()
