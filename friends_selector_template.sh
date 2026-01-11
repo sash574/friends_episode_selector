26cfad8bd4dee2988abe76f40929295524673cef
sash574 <sash574@nomail.de>
2026-01-11 15:25:04 +0100

    shell script to run the program

---
+++ friends_selector_template.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source ..../friends_venv/bin/activate         # add path-to-your-virtual-environment in placeholder; activates it
+cd ..../friends_episode_selector/             # add path to the repository/directory to navigate there
+python3 episode_selector.py                   # run the program
